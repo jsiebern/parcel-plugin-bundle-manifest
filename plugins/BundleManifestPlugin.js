94632004d68a3c058c7539d21dbdf29cd78848c9
Jonathan Siebern <jsiebern88@gmail.com>
2018-02-13 20:51:31 +0100

    Fix output in dev mode

--- plugins/BundleManifestPlugin.js
+++ plugins/BundleManifestPlugin.js
@@ -1,34 +1,10 @@
-function stringify(obj, replacer, spaces, cycleReplacer) {
-  return JSON.stringify(obj, serializer(replacer, cycleReplacer), spaces)
-}
-
-function serializer(replacer, cycleReplacer) {
-  var stack = [], keys = []
-
-  if (cycleReplacer == null) cycleReplacer = function (key, value) {
-    if (stack[0] === value) return "[Circular ~]"
-    return "[Circular ~." + keys.slice(0, stack.indexOf(value)).join(".") + "]"
-  }
-
-  return function (key, value) {
-    if (stack.length > 0) {
-      var thisPos = stack.indexOf(this)
-      ~thisPos ? stack.splice(thisPos + 1) : stack.push(this)
-      ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key)
-      if (~stack.indexOf(value)) value = cycleReplacer.call(this, key, value)
-    }
-    else stack.push(value)
-
-    return replacer == null ? value : replacer.call(this, key, value)
-  }
-}
-
 const path = require('path');
 const fs = require('fs');
 const hasha = require('hasha');
 
 module.exports = function (bundler) {
   const logger = bundler.logger;
+  const isProduction = bundler.options.production || process.env.NODE_ENV === 'production';
 
   /**
    * Read the paths already registered within the manifest.json
@@ -61,14 +37,18 @@ module.exports = function (bundler) {
     const dir = asset.options.outDir;
 
     let basename = asset.generateBundleName();
+
     if (type === 'css') {
-      basename = `${rootName}.css`;
+      basename = rootName + '.css';
     }
 
-    const origPath = path.join(dir, basename);
-    const hash = hasha(asset.hash + Array.from(bundle.assets).map(asset => asset.hash).join(''), { algorithm: 'md5' });
-
-    manifest[basename] = `${rootName}.${hash}.${type}`;
+    if (!isProduction) {
+      manifest[basename] = basename;
+    }
+    else {
+      const hash = hasha(asset.hash + Array.from(bundle.assets).map(asset => asset.hash).join(''), { algorithm: 'md5' });
+      manifest[basename] = `${rootName}.${hash}.${type}`;
+    }
 
     bundle.childBundles.forEach((bundle) => {
       addToManifest(bundle, manifest, rootName);
@@ -87,16 +67,18 @@ module.exports = function (bundler) {
     const rootName = rootFile.slice(0, rootFile.length - 1).join('.');
 
     let newManifest = addToManifest(bundle, {}, rootName);
-    Object.keys(newManifest).forEach(fName => {
-      const p = path.join(dir, fName);
-      const pNew = path.join(dir, newManifest[fName]);
-      let c = fs.readFileSync(p, 'utf8');
+    if (isProduction) {
       Object.keys(newManifest).forEach(fName => {
-        c = c.replace(fName, newManifest[fName]);
+        const p = path.join(dir, fName);
+        const pNew = path.join(dir, newManifest[fName]);
+        let c = fs.readFileSync(p, 'utf8');
+        Object.keys(newManifest).forEach(fName => {
+          c = c.replace(fName, newManifest[fName]);
+        });
+        fs.writeFileSync(p, c);
+        fs.renameSync(p, pNew);
       });
-      fs.writeFileSync(p, c);
-      fs.renameSync(p, pNew);
-    });
+    }
 
     logger.status('📦', 'PackageManifestPlugin');
     logger.status('📄', `manifest : ${manifestPath}`);
